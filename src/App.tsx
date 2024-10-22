53dfa11c01e9dc22996ad32df7e677b09429e268
Gustavo Cardoso <gustavo.che@live.fr>
2024-10-22 16:24:14 +0200

    pokedex-bulba

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import PokemonCard from "./components/PokemonCard";
+
+function App() {
+  return (
+    <section>
+      <PokemonCard />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,6 @@
+function PokemonCard() {
+  return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img></figure>;
+  
+}
+
+export default PokemonCard;
