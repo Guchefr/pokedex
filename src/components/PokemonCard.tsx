97208e0407895d43ce277e6303082ae883ac9bf4
Gustavo Cardoso <gustavo.che@live.fr>
2024-10-24 11:37:09 +0200

    component2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,30 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    imgSrc: null,
+  },
+];
+
+
 function PokemonCard() {
-  return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img></figure>;
-  
+
+  const pokemon = pokemonList[0];
+  return (
+    <figure>
+    
+    {pokemon.imgSrc ? (
+      <img src={pokemon.imgSrc} alt={pokemon.name}></img>
+    ) : (
+      <p>???</p>
+    )}
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>
+);
 }
 
 export default PokemonCard;
