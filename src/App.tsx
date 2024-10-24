9051d58a4b1940e0027cff58869481ec10228108
Gustavo Cardoso <gustavo.che@live.fr>
2024-10-24 15:14:03 +0200

    quete4

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    imgSrc: null,
+  },
+];
+
 function App() {
   return (
     <section>
-      <PokemonCard />
-    </section>
+      <PokemonCard pokemon={pokemonList[0]} />
+      </section>
   );
 }
 
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,17 @@
-function PokemonCard() {
-  return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img></figure>;
+function PokemonCard({pokemon}) {
+
   
+  return (
+    <figure>
+    
+    {pokemon.imgSrc ? (
+      <img src={pokemon.imgSrc} alt={pokemon.name}></img>
+    ) : (
+      <p>???</p>
+    )}
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>
+);
 }
 
 export default PokemonCard;
